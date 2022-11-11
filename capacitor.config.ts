ad599090e92b98d16a5c5d721a8eb0fd82641d8f
Fadel-Dev <diopfadel06@gmail.com>
2022-11-11 11:27:01 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sama deukouway',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
