20a30273ae1d26f911bf5823ec2190364943dfb2
fadel diop <diopfadel06@gmail.com>
2022-12-23 14:33:21 +0000

    end before uploading

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { Router } from '@angular/router';
 import { Observable,  } from 'rxjs';
+
+
 
 
 @Component({
@@ -16,11 +20,32 @@ export class Tab1Page {
   items:Observable<any[]>
   constructor(
     public firestore:AngularFirestore,
-    public router:Router
+    public router:Router,
+    public afDB: AngularFireDatabase,
+    public afSG: AngularFireStorage
 
-  ) { 
+  )  { 
    this.items= this.firestore.collection('house').valueChanges();
   } 
+  // RECUPE IMAGE
+
+   getImagesDatabase() {
+  
+  this.afDB.list('Images').snapshotChanges(['child_added']).subscribe(images => {
+    images.forEach(image => {
+      console.log('Image: ' + image.payload.exportVal().name);
+    });
+  });
+}
+
+getImagesStorage() {
+  // pour récupérer l'URL des images
+  // pour récupérer les informations des images
+  this.afDB.list('Images').snapshotChanges(['child_added']).subscribe(images => {
+    console.log(images);
+  });
+  
+}
  passName(){
   
     this.router.navigate(['/description',this.cool])
